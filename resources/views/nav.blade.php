7722dfc79c53828055acf5260de39249516e4dfb
blueswordleo <117076077+blueswordleo@users.noreply.github.com>
2022-11-08 07:56:12 +0700

    Update nav.blade.php

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -4,17 +4,6 @@
       <div class="navbar-nav mx-auto">
         <a class="nav-item nav-link active" href="#">About Malang <span class="sr-only">(current)</span></a>
         <a class="nav-item nav-link" href="/">Home</a>
-        <div class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Information
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </div>
       </div>
     </div>
   </nav>
